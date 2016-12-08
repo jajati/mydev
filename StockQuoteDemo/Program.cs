b09c0f494c59f7c5cf8b5a164779c167fdbdb419
jajati <censoft.pune44@gmail.com>
2016-12-08 11:39:30 +0530

    Add files via upload

---
+++ StockQuoteDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Json;
+using Newtonsoft.Json.Linq;
+
+namespace StockQuoteDemo
+{
+    class Program
+    {
+        public static string stockPrice(string stck)
+        {
+
+             string csvData;
+            string stock = stck ;
+
+
+            //string nationalStock;
+            string globalstock;
+           // stock = Console.ReadLine();
+
+            using (WebClient web = new WebClient())
+            {
+                               
+
+                  globalstock = "http://finance.google.com/finance/info?client=ig&q="  + stock;
+               
+                     csvData = web.DownloadString(globalstock);
+                
+                            }
+            string value = csvData.Replace("//", "").Replace("[", "").Replace("]", "");
+          
+            JObject response = JObject.Parse(value);
+            String CName = Convert.ToString(response["t"]);
+            String stockMarketType = Convert.ToString(response["e"]);
+            String l = Convert.ToString(response["l"]);
+            String l_fix = Convert.ToString(response["l_fix"]);
+            String l_cur = Convert.ToString(response["l_cur"]);
+           // String pcls_fix = Convert.ToString(response["pcls_fix"]);
+            string lt_dts = Convert.ToString(response["lt_dts"]);
+            string gain = Convert.ToString(response["c"]);
+            string cp = Convert.ToString(response["cp"]);
+         //   Console.WriteLine(CName, stockMarketType, l,l_fix, l_cur,  lt_dts,gain,cp);
+           // Console.ReadLine();
+
+
+
+        
+
+            return value;
+        }
+        static void Main(string[] args)
+        {
+            string output = Program.stockPrice("IBM");
+          Console.WriteLine(output);
+          Console.ReadLine();
+
+        }
+    }
+}
+
---
+++ StockQuoteDemo/YahooFinance.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+//namespace StockQuoteDemo
+//{
+//    public static class YahooFinance
+//    {
+//        public static List<Price> Parse(string csvData)
+//        {
+//            List<Price> prices = new List<Price>();
+
+//            string[] rows = csvData.Replace("\n", "").Split('\t');
+
+//         //   string json = JsonConvert.SerializeObject(rows.ToArray());
+
+
+            
+//            foreach (string row in rows)
+//            {
+//                if (string.IsNullOrEmpty(row)) continue;
+               
+               
+
+//                string[] cols = row.Split(',');
+
+//              Price p = new Price();
+//            //    //p.Symbol = cols[0];
+//            //    //p.Name = cols[1];
+//            //    //p.Bid = Convert.ToDecimal(cols[2]);
+//            //    //p.Ask = Convert.ToDecimal(cols[3]);
+//            //    //p.Open = Convert.ToDecimal(cols[4]);
+//            //    //p.PreviousClose = Convert.ToDecimal(cols[5]);
+//            //    //p.Last = Convert.ToDecimal(cols[6]);
+
+
+//              p.t = cols[1];
+//              p.e = cols[2];
+//              p.l = Convert.ToDecimal((cols[3].Split(':')[1]).Replace("'\'",""));
+//              p.l_fix = Convert.ToDecimal(cols[4]);
+//              p.l_cur = Convert.ToDecimal(cols[5]);
+//             // p.PreviousClose = cols[5];
+//              p.pcls_fix = Convert.ToDecimal(cols[12]);
+
+//                prices.Add(p);
+//            }
+
+//            return prices;
+//        }
+//    }
+
+//    public class Price
+//    {
+        
+//        public string t { get; set; }
+//        public string e { get; set; }
+//        public decimal l { get; set; }
+//        public decimal l_fix { get; set; }
+//        public decimal l_cur { get; set; }
+//        public decimal PreviousClose { get; set; }
+//        public decimal pcls_fix { get; set; }
+       
+//    }
+//}
